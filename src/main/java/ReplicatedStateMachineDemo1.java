a3e08b76ff8d75e3d52bca01c373a06a27ccb11a
elale <910159664@qq.com>
2017-12-29 22:20:35 -0800

    init raft demo run test ------------------------- xxx -name A xxx -name B

---
+++ src/main/java/ReplicatedStateMachineDemo1.java
@@ -0,0 +1,8 @@
+import org.jgroups.raft.demos.ReplicatedStateMachineDemo;
+
+public class ReplicatedStateMachineDemo1 {
+    public static void main(String[] args) throws Exception {
+        ReplicatedStateMachineDemo.main(args);
+    }
+
+}
